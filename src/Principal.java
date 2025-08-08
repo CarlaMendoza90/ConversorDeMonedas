de271ce3d4ea9a12c83c6a31041c25927e76c012
CarlaMendoza90 <mendozacoronadocarla@gmail.com>
2025-08-08 20:16:04 -0300

    [NUEVO] Acceso a la API

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,21 @@
-public class Principal {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
+public class Principal {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String API_KEY = System.getenv("API_KEY");
+        String direccion = "https://v6.exchangerate-api.com/v6/"+API_KEY+"/pair/EUR/GBP";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
